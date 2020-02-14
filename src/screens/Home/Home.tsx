984f663d42623d3551119b7949aa40ec8034b213
Hygor <hygor@x-apps.com.br>
2020-02-14 14:47:44 -0300

    feat(home): adding home screen
    Added the home screen base structure

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,15 +1,29 @@
+// @ts-nocheck
+
 import React from 'react'
+import Header from '~/components/Header'
 
 import { Container } from './styles'
+import { lorem } from '~/utils/dev'
+import { useSelector } from 'react-redux'
 
 type HomeProps = {
 
 }
 
 function Home (props: HomeProps) {
+  const { headerFixed } = useSelector(({ app }) => app)
+
   return (
     <Container>
-      Home
+      <Header />
+      {headerFixed && <div className="extra-size" />}
+      <div className="fake-content">
+        <p style={{ color: '#ffa5a5' }}>{lorem.large}</p>
+        <p style={{ color: '#ffdb9a' }}>{lorem.large}</p>
+        <p style={{ color: '#abff94' }}>{lorem.large}</p>
+        <p style={{ color: '#a3e0ff' }}>{lorem.large}</p>
+      </div>
     </Container>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,25 @@
 import styled from 'styled-components'
+import Scroll from '~/components/Scroll'
 
-export const Container = styled.div`
+export const Container = styled(Scroll)`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  position: relative;
+  overflow: hidden;
   
+  .fake-content{
+    height: 300vh;
+    padding-top: ${({ fixed }) => fixed ? 240 : 0}px;
+    
+    p{
+      margin: 20px;
+    }
+  }
+  
+  .extra-size{
+    display: block;
+    height: 240px;
+  }
 `;
+
