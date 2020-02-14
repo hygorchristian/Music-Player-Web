1a93632b75f404e982522a33f8af9d26edef14ec
Hygor <hygor@x-apps.com.br>
2020-02-14 14:43:23 -0300

    feat(header): creating header base behavior and style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,57 @@
-import React from 'react';
+// @ts-nocheck
 
-import { Container } from './styles';
+import React, { useEffect, useState, useRef } from 'react'
+import { Container, SearchContainer, Fixed } from './styles'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppActions } from '~/store/ducks/app'
 
 type HeaderProps = {
 
 }
 
-function Header(props: HeaderProps) {
-  return <Container />
+function Search (props) {
+  return (
+    <SearchContainer {...props}>
+      search
+    </SearchContainer>
+  )
 }
 
-export default Header;
+function Header (props: HeaderProps) {
+  const dispatch = useDispatch()
+
+  const { scrollTop, headerFixed } = useSelector(({ app }) => app)
+  const [bottom, setBottom] = useState(500)
+
+  const containerEl = useRef(null)
+
+  useEffect(() => {
+    const bot = containerEl.current && containerEl.current.getBoundingClientRect().bottom
+    // console.log(bot)
+    setBottom(bot)
+
+    dispatch(AppActions.setHeaderFixed(bottom <= 116 && scrollTop >= 116))
+  }, [scrollTop])
+
+  return (
+    <Container
+      ref={containerEl}
+      fixed={headerFixed}
+      overlay={scrollTop > 90 ? 1 : scrollTop / 90}
+    >
+      <div className="gradient" />
+      <div className="overlay" />
+      <div className="content">
+        <div className={`header-info ${headerFixed && 'fixed'}`}>
+          <h3>Home</h3>
+        </div>
+        <div className={`subheader-info ${headerFixed && 'fixed'}`}>
+          <h4>Home</h4>
+        </div>
+      </div>
+      <Search bgOpacity={scrollTop > 80 ? 1 : 0} />
+    </Container>
+  )
+}
+
+export default Header
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,94 @@
+// @ts-nocheck
+
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: ${({ fixed }) => fixed ? 116 : 240}px;
+  max-height: ${({ fixed }) => fixed ? 116 : 1000}px;
+  position: ${({ fixed }) => fixed ? 'fixed' : 'relative'}; 
+  border-bottom:${({ fixed }) => fixed ? '1px solid #282828' : 'none'};
   
-`;
+  .overlay{
+    height: 100%;
+    width: 100%;
+    background-color: #121212;
+    position: absolute;
+    opacity: ${({ overlay }) => overlay};
+  }
+  
+  .gradient{
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-end;
+    padding-bottom: 52px;
+    padding-left: 32px;
+    position: absolute;
+    background-image: linear-gradient(#414141, #181818);
+  }
+  
+  .content{
+    display: flex;
+    flex-direction: row;
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    
+    .header-info{
+      position: absolute;
+      bottom: 0;
+      display: flex;
+      padding-left: 32px;
+      transition: all 200ms ease-in-out;
+      
+      &.fixed{
+        bottom: 300px;
+      }
+      
+      h3{
+        font-size: 48px;
+        font-weight: 800;
+        margin-bottom: 40px;
+      }
+    }
+    
+    .subheader-info{
+      position: absolute;
+      bottom: -100px;
+      padding-left: 32px;
+      transition: all 150ms ease-in-out;
+      
+      &.fixed{
+        bottom: 0;
+      }
+      
+      h4{
+        font-size: 28px;
+        font-weight: 800;
+        margin-bottom: 18px;
+      }
+    }
+  }
+`
+
+export const Fixed = styled.div`
+  height: 116px;
+  background-color: red;
+`
+
+export const SearchContainer = styled.div`
+  height: 48px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0 20px;
+  position: fixed; 
+  background: linear-gradient(rgba(18,18,18,${({ bgOpacity }) => bgOpacity}) 0%, rgba(18,18,18,${({ bgOpacity }) => bgOpacity}) 70%, rgba(18,18,18,0) 100%);
+`
