f13738c46e64591f95b1b7c66fa57765dd84bb4f
Hygor <hygor@x-apps.com.br>
2020-02-15 11:49:19 -0300

    feat(header): creating header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,15 +4,66 @@ import React, { useEffect, useState, useRef } from 'react'
 import { Container, SearchContainer, Fixed } from './styles'
 import { useDispatch, useSelector } from 'react-redux'
 import { AppActions } from '~/store/ducks/app'
+import { ReactSVG } from 'react-svg'
+import { useTabletMode, useWindowSize } from '~/hooks'
 
 type HeaderProps = {
 
 }
 
 function Search (props) {
+  const { width } = useWindowSize()
+  const { isTablet } = useTabletMode()
+
+  console.log({ width })
+
+  const [value, setValue] = useState('')
+  const [headerWidth, setHeaderWidth] = useState(width - 456)
+
+  const handleChange = (e: React.ChangeEvent) => {
+    const newValue = e.target.value
+    setValue(newValue)
+  }
+
+  useEffect(() => {
+    const newVal = isTablet ? width - 200 : width - 456
+    setHeaderWidth(newVal)
+  }, [width])
+
   return (
-    <SearchContainer {...props}>
-      search
+    <SearchContainer {...props} width={headerWidth}>
+      <div className="search-control">
+        <button style={{ marginRight: 8 }}>
+          <ReactSVG src="/icons/spotify/chevron-left.svg" />
+        </button>
+        <button>
+          <ReactSVG src="/icons/spotify/chevron-right.svg" />
+        </button>
+        <div className="search-input">
+          <ReactSVG src="/icons/spotify/search.svg" />
+          <input
+            placeholder="Search"
+            value={value}
+            onChange={handleChange}
+          />
+          {(value && value.length > 0) && (
+            <button onClick={() => setValue('')}>
+              <ReactSVG src="/icons/spotify/x.svg" />
+            </button>
+          )}
+        </div>
+      </div>
+      <div className="user-control">
+        <div className="user-info">
+          <ReactSVG src="/icons/spotify/user.svg" />
+          <div className="username">
+            <span>rushy06</span>
+          </div>
+        </div>
+        <button>
+          <ReactSVG src="/icons/spotify/chevron-down.svg" />
+        </button>
+      </div>
     </SearchContainer>
   )
 }
@@ -27,7 +78,6 @@ function Header (props: HeaderProps) {
 
   useEffect(() => {
     const bot = containerEl.current && containerEl.current.getBoundingClientRect().bottom
-    // console.log(bot)
     setBottom(bot)
 
     dispatch(AppActions.setHeaderFixed(bottom <= 116 && scrollTop >= 116))
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,6 +1,6 @@
 // @ts-nocheck
 
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
@@ -79,16 +79,133 @@ export const Container = styled.div`
 
 export const Fixed = styled.div`
   height: 116px;
-  background-color: red;
 `
 
 export const SearchContainer = styled.div`
   height: 48px;
-  width: 100%;
+  width: ${({ width }) => width}px;
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding: 0 20px;
+  justify-content: space-between;
   position: fixed; 
   background: linear-gradient(rgba(18,18,18,${({ bgOpacity }) => bgOpacity}) 0%, rgba(18,18,18,${({ bgOpacity }) => bgOpacity}) 70%, rgba(18,18,18,0) 100%);
+
+  .search-control{
+    margin-left: 14px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  
+    & > button{
+      height: 24px;
+      width: 24px;
+      
+      &:active{
+        svg{
+          stroke: #A8A8A8;
+        }
+      }
+      
+      svg{
+        stroke: white;
+        width: 14px;
+        height: 14px;
+      }
+    }
+    
+    .search-input{
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      height: 24px;
+      width: 175px;
+      border-radius: 24px;
+      background-color: #ffffff;
+      margin-left: 14px;
+      padding-left: 6px;
+      overflow: hidden;
+      
+      svg{
+        stroke: #000000;
+        height: 14px;
+        width: 14px;
+        margin-top: 4px;
+      }
+      
+      input{
+        border: none;
+        padding-left: 6px;
+        height: 100%;
+        font-size: 12px;
+        width: 130px;
+        
+        &::placeholder{
+          color: #000000;
+        }
+      }
+      
+      & > button{
+        height: 24px;
+        width: 24px;
+        
+        svg{
+          stroke: #C8C8C8;
+          height: 10px;
+          width: 10px;
+        }
+      }
+    }
+  }
+  
+  .user-control{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-right: 35px;
+    
+    .user-info{
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      
+      & svg{
+        height: 22px;
+        width: 22px;
+        stroke: #ffffff;
+      }
+      
+      .username{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin-left: 10px;
+        margin-right: 20px;
+        
+        span{
+          color: #ffffff;
+          font-size: 14px;
+          line-height: 10px;
+        }
+      }
+      
+    }
+    
+    button{
+      height: 24px;
+      width: 24px;
+      
+      &:active{
+        svg{
+          stroke: #A8A8A8;
+        }
+      }
+      
+      svg{
+        stroke: white;
+        width: 14px;
+        height: 14px;
+      }
+    }
+  }
 `
