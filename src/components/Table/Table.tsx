f0096d6718236931a7505919b1035c9fe7ac7b63
Hygor <hygor@x-apps.com.br>
2020-02-18 12:52:18 -0300

    feat(component): adds table component to the project

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+type TableProps = {
+
+}
+
+function Table(props: TableProps) {
+  return <Container />
+}
+
+export default Table;
