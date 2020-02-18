c05e160420a83765642e9e9a18ad0a659f9ec42b
Hygor <hygor@x-apps.com.br>
2020-02-18 12:48:58 -0300

    feat(component): add switch spotify component

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+type CheckboxProps = {
+
+}
+
+function Checkbox(props: CheckboxProps) {
+  return <Container />
+}
+
+export default Checkbox;
